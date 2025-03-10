890b36658b174cafc595c7c41ba492e1a7410703
Samarth Gaikwad <samarthgaikwad516@gmail.com>
2025-03-10 16:58:43 +0530

    Update main.tf

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -1,3 +1,44 @@
 resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
 }
+
+resource "aws_s3_bucket_website_configuration" "site" {
+  
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.static_site.id
+  key    = "index.html"
+  source = "index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_policy" "public_access" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy =  jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow"
+        Principal = "*"
+        Action   = "s3:GetObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.static_site.bucket}/*"
+      }
+    ]
+  })
+}
