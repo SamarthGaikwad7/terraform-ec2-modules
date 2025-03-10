5025a4ea042d330e99a3f688c50c6152bda42aaa
Samarth Gaikwad <samarthgaikwad516@gmail.com>
2025-03-10 17:14:13 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,8 +12,4 @@ output "s3_bucket_name" {
   value = module.s3.bucket_name
 }
 
-output "website_url" {
-  value = aws_s3_bucket_website_configuration.site.website_endpoint
-  description = "URL of the hosted static website"
-}
 
