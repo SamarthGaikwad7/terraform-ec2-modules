fb120f9cf8c52af056241f8d0dbe31a0e05f24c8
Samarth Gaikwad <samarthgaikwad516@gmail.com>
2025-03-10 17:21:54 +0530

    Update main.tf

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -14,11 +14,12 @@ resource "aws_s3_bucket_website_configuration" "site" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.example.id
 
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
 
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.example.id
